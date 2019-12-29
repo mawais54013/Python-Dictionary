aa797a5e3b67bb3d8c646757c24297dc45044eff
MAwais <mawais54013@outlook.com>
2019-12-28 22:29:33 -0800

    practice python

--- app1.py
+++ app1.py
@@ -1,9 +1,16 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(word1):
-    return data[word1]
+    word1 = word1.lower()
+    if word1 in data:
+        return data[word1]
+    elif len(get_close_matches(word1, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word1, data.keys())[0]
+    else:
+        return "error word does not exist"
 
 word = input("Enter word: ")
 
