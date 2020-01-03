d88a79bdb27b84037f5409525f61bcbd8757eeeb
MAwais <mawais54013@outlook.com>
2020-01-02 19:20:49 -0800

    add statement

--- app1.py
+++ app1.py
@@ -8,7 +8,13 @@ def translate(word1):
     if word1 in data:
         return data[word1]
     elif len(get_close_matches(word1, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(word1, data.keys())[0]
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word1, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(word1, data.keys())[0]]
+        elif yn == "N":
+            return "Word does not exist"
+        else:
+            return "Error, we didn't understand your input"
     else:
         return "error word does not exist"
 
